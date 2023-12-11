97f167973001b1ee565c6cf18c8e9f4dccac1e41
vincent93113 <30742232+vincent93113@users.noreply.github.com>
2023-12-11 23:08:26 +0800

    update download function and error handling

--- hm_man.py
+++ hm_man.py
@@ -19,55 +19,70 @@ api1_data = api1_data.json()
 api2_data = api2_data.json()
 api3_data = api3_data.json()
 
-def download(url):                     
-                          
-    jpg = requests.get(url,headers=headers)           
-    f = open(route, 'wb')    
-    f.write(jpg.content)                 
-    f.close()
+def download(url, route):                     
+    jpg = requests.get(url, headers=headers)           
+    with open(route, 'wb') as f:
+        f.write(jpg.content)
 
 
 data_list =[]
 contail_url = []
 material_list =[]
-name = 1
+name_number = 1
 all_api = [api1_data['products'],api2_data['products'],api3_data['products']]
 for each_api in all_api:
 
     for each_data in each_api:
 
         try:
-            each_url = f'https://www2.hm.com'+each_data['swatches'][0]['articleLink']
+            each_url = f'https://www2.hm.com'+str(each_data['swatches'][0]['articleLink'])
+            print(f' {name_number} url ok')        
         except:
             each_url = 'None'
+            print(f' {name_number} url error')
+
+        contail_url.append(each_url)
 
         try:
             title = each_data['title']
+            print(f' {name_number} title ok') 
         except:
             title = 'None'
+            print(f' {name_number} title error')
 
         try:
             price = each_data['price']
+            print(f' {name_number} price ok')
+
         except:
             price = 'None'
+            print(f' {name_number} price error')
+
 
         try:
             color = each_data['swatches'][0]['colorName']
+            print(f' {name_number} color ok')
+
         except:
             color = 'None'
+            print(f' {name_number} color error')
+
         
-        contail_url.append(each_url)
+
         
+        route = f'image/hm/man/{name_number}.jpg'
         try:
-            photo = 'https:'+each_data['image'][0]['dataAltImage']
-            download(photo)
+            photo = 'https:'+str(each_data['image'][0]['dataAltImage'])
+            download(photo,route)
+            print(f' {name_number} download ok')
+
         except:
             photo = 'None'
+            print(f' {name_number} download error')
+
         
-        route = f'image/hm/man/{name}.jpg'
-        
-        name = name +1
         data_list.append([title,price,color,route])
+        name_number = name_number +1
 
 
     for each_contail in contail_url:
@@ -97,4 +112,4 @@ for each_api in all_api:
 df = pd.DataFrame(data_list, columns=['名稱', '價格', '顏色','路徑'])
 df.insert(3, '成分', contail_list[:len(data_list)])
 df.insert(4, '描述', material_list[:len(data_list)])
-df.to_csv('output.csv', index=False, encoding='utf-8')
+df.to_csv('output_man.csv', index=False, encoding='utf-8')
